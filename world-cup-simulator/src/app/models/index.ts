0c60e6cb9de327f24576e83efcedd23a2e730774
MrLizard11 <ajammel9@gmail.com>
2025-08-12 15:58:06 -0400

    feat: add tournament statistics models

--- world-cup-simulator/src/app/models/index.ts
+++ world-cup-simulator/src/app/models/index.ts
@@ -1,3 +1,4 @@
 export { Team } from './team.model';
 export { Match } from '../models/group.model';
+export { TournamentStats, TeamPerformance, ChampionJourneyMatch, ChampionJourney } from './tournament-stats';
 
